8c45aabd4466640af4618bd6f0068768ef95883f
Andrew H <aho338@gmail.com>
2016-04-29 12:27:10 -0700

    Remove unneeded jsDOM test files

--- test/entry.js
+++
@@ -1,8 +0,0 @@
-var setup = require('./setup.js');
-
-// Add all test files below
-// Client-side tests
-var appTest = require('./components/App.test.js');
-
-// Server-side tests
-var trueTest = require('./server/true.test.js');
