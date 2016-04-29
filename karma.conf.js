d566a515fb8c2447f2545a365e5511026e44f704
Andrew H <aho338@gmail.com>
2016-04-29 12:26:35 -0700

    Add karma configuration files

---
+++ karma.conf.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+
+module.exports = function (config) {
+  config.set({
+    browsers: [ process.env.CONTINUOUS_INTEGRATION ? 'Firefox' : 'Chrome' ], // use Firefox for Travis CI
+    singleRun: true,
+    frameworks: [ 'mocha' ],
+    files: [
+      'tests.webpack.js'
+    ],
+    preprocessors: {
+      'tests.webpack.js': [ 'webpack', 'sourcemap' ]
+    },
+    reporters: [ 'dots' ],
+    webpack: {
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [
+          {
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+              presets: ['es2015', 'react']
+            }
+          }
+        ]
+      }
+    },
+    webpackServer: {
+      noInfo: true
+    }
+  });
+};
