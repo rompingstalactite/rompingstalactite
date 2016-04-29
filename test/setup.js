984c1ca898d8ebfe20958bdbd2318f5594f0e538
Andrew H <aho338@gmail.com>
2016-04-29 12:27:10 -0700

    Remove unneeded jsDOM test files

--- test/setup.js
+++
@@ -1,6 +0,0 @@
-var jsdom = require('jsdom');
-// A super simple DOM ready for React to render into
-// Store this DOM and the window in global scope ready for React to access
-global.document = jsdom.jsdom('<!doctype html><html><body><div id="app"></div></body></html>');
-global.window = document.defaultView;
-global.navigator = global.window.navigator;
