aad05acfd79caf1965430d4ccec9731c41f6e66e
Andrew H <aho338@gmail.com>
2016-04-30 16:02:11 -0700

    Refactor App to use Redux

--- client/components/App.js
+++ client/components/App.js
@@ -3,35 +3,57 @@ import { connect } from 'react-redux';
 import actions from '../actions/index.js';
 import '../scss/app.scss';
 
-import Nav from '../../client/components/Nav.js';
-import Profile from '../../client/components/Profile.js';
-import RecipeContainer from '../../client/components/RecipeContainer.js';
+import Nav from './Nav.js';
+import Profile from './Profile.js';
+import RecipeContainer from './RecipeContainer.js';
 
 class App extends Component {
   render() {
+    const { profile, recipesOwned, recipesFollowed } = this.props;
     return (
       <div>
         <Nav />
-        <Profile profile={{ avatar: 'http://www.carderator.com/assets/avatar_placeholder_small.png', username: 'USERNAME' }} />
+        <Profile profile={profile} />
+        <h1>Dashboard</h1>
         <RecipeContainer
           className="recipes-owned"
           type="My Owned Recipes"
-          recipes={[
-            { name: 'Owned Recipe 1' },
-            { name: 'Owned Recipe 2' },
-            { name: 'Owned Recipe 3' }]}
+          recipes={recipesOwned}
         />
         <RecipeContainer
           className="recipes-followed"
           type="My Followed Recipes"
-          recipes={[
-            { name: 'Followed Recipe 1' },
-            { name: 'Followed Recipe 2' },
-            { name: 'Followed Recipe 3' }]}
+          recipes={recipesFollowed}
         />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = function (state) {
+  return {
+    profile: state.profile,
+    recipesOwned: state.recipesOwned,
+    recipesFollowed: state.recipesFollowed,
+  };
+};
+
+// const mapDispatchToProps = function (dispatch) {
+//   return {
+//    // fill in any actions here
+//   };
+// };
+
+App.propTypes = {
+  recipesOwned: PropTypes.array.isRequired,
+  recipesFollowed: PropTypes.array.isRequired,
+  profile: PropTypes.shape({
+    avatar: PropTypes.string,
+    username: PropTypes.string,
+  }).isRequired,
+};
+
+export default connect(
+  mapStateToProps
+  // mapDispatchToProps,
+)(App);
