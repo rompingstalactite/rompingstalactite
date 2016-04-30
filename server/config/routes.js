2a1347947f62dfc1ba5be7f7627b03bdf7e90149
thomasingalls <thomasingalls@gmail.com>
2016-04-29 20:29:40 -0700

    Update middleware, routes, server to full es6 syntax

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,10 +1,4 @@
-// var linksController = require('../links/linkController.js');
-// var userController = require('../users/userController.js');
-// var helpers = require('./helpers.js'); // our custom middleware
+import uc from '../APIv1/users/userController.js';
 
-module.exports = function(app, express) {
-  app.get('/', function(req, res, next) {
-    res.json({"totally": "working"});
-    next();
-  });
-}
+module.exports = (app, express) => {
+};
