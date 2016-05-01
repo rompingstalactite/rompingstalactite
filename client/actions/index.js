82da773ca3350eddbb6a23f6966bf5dfa5cc70da
controtie <dylan.ad.tran@gmail.com>
2016-05-01 15:53:15 -0700

    change actions index to es6 method style

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,7 +1,7 @@
 import * as types from '../constants/ActionTypes.js';
 
 module.exports = {
-  updateRecipe: function(recipe) {
+  updateRecipe(recipe) {
     return { type: types.UPDATE_RECIPE, recipe };
   },
 };
