25a080f5fc87080f1053545a5ada33fc96bfed72
thomasingalls <thomasingalls@gmail.com>
2016-05-02 16:28:02 -0700

    Update getOneUser, remove duplicate getAllUsers

--- server/APIv1/users/userController.js
+++ server/APIv1/users/userController.js
@@ -38,12 +38,33 @@ module.exports = {
   },
 
   getOneUser: (request, response, next) => {
-    response.json({ totally: 'working '});
-  },
-
-  getAllUsers: (request, response, next) => {
-    response.status(403);
+    /**
+     * get one value out of the database
+     * http://vitaly-t.github.io/pg-promise/Database.html#.one
+     *
+     * @param  {object}  query object
+     * @param  {array}   array of values, mapped to $1, $2, ...
+     *                     in querystring, indexed on 1
+     *
+     * @return {Promise}  when 1 row is returned, it resolves with that row as a single object;
+     *                    when no rows are returned, it rejects with QueryResultError = No data returned from the query.
+     *                    when multiple rows are returned, it rejects with QueryResultError = Multiple rows were not expected.
+     */
+    db.one({
+      name: 'find-user',
+      text: 'select * from users where id = $1',
+      values: [request.params.user_id],
+    })
+      .then((data) => {
+        response.json(data);
     next();
+      })
+      .catch((error) => {
+        response.json({
+          error: `Error code: ${error.code}, Error message: ${error.detail}`,
+        });
+        next();
+      });
   },
 
   // getCurrentUser: function () {
