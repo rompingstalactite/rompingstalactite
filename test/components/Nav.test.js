440689de6d5bd3cf922ddcd2c179cf5413fbfff3
Andrew H <aho338@gmail.com>
2016-05-02 15:19:12 -0700

    Refactor component tests to use enzyme

--- test/components/Nav.test.js
+++ test/components/Nav.test.js
@@ -1,12 +1,12 @@
 const expect = require('chai').expect;
 const React = require('react');
-const TestUtils = require('react-addons-test-utils'); // Alternately could use the DOM API
+import { mount } from 'enzyme';
 
 import Nav from '../../client/components/Nav.js';
 
-describe('nav', () => {
+describe('<Nav />', () => {
   it('renders without problems', () => {
-    const nav = TestUtils.renderIntoDocument(<Nav />);
-    expect(nav).to.not.be.undefined;
+    const wrapper = mount(<Nav />);
+    expect(wrapper.find(Nav)).to.have.length(1);
   });
 });
