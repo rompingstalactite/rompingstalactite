197411fb691d83d7464f6df7ccc940b549939edc
controtie <dylan.ad.tran@gmail.com>
2016-05-04 19:06:48 -0700

    add Dashboard template and tests

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import RecipeEntry from './RecipeEntry';
 
 const Dashboard = (props) => (
   <div className="dashboard">
     <h1>THIS IS THE DASHBOARD</h1>
   </div>
 );
+
 
 export default Dashboard;
--- test/components/Dashboard.test.js
+++ test/components/Dashboard.test.js
@@ -1,12 +1,22 @@
 const expect = require('chai').expect;
 const React = require('react');
+import { Provider } from 'react-redux';
 import { mount } from 'enzyme';
+import fakeStore from './fakeStore';
 
 import Dashboard from '../../client/components/Dashboard.js';
 
 describe('<Dashboard />', () => {
+  let wrapper;
+
+  beforeEach('render Profile', () => {
+    wrapper = mount(
+      <Provider store={fakeStore}>
+        <Dashboard />
+      </Provider>);
+  });
+
   it('should render without problems', () => {
-    const wrapper = mount(<Dashboard />);
     expect(wrapper.find(Dashboard)).to.have.length(1);
   });
 });
