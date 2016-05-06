aad6ce74c738d519133ab683b8b14faa10ec8ef9
Andrew H <aho338@gmail.com>
2016-05-06 10:20:18 -0700

    Refactor to ES6

--- client/utils/utils.js
+++ client/utils/utils.js
@@ -1,74 +1,73 @@
 require('isomorphic-fetch');
 
-var fetchRecipe = function(recipe, callback) {
-  fetch('http://localhost:8080/api/v1/recipes/' + recipe, {
+const fetchRecipe = (recipe, callback) => {
+  fetch(`http://localhost:8080/api/v1/recipes/${recipe}`, {
     method: 'GET',
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
     },
   })
-  .then(function(response) {
+  .then((response) => {
     if (response.status >= 400) {
       throw new Error('Bad response from server');
     }
     return response.json();
   })
-  .then(function(data) {
+  .then((data) => {
     callback(data);
   })
-  .catch(function(error) {
+  .catch((error) => {
     console.log('Error:', error);
     return;
   });
 };
 
-var bindAuthorToNewRecipe = function(author) {
-  return assignAuthorToNewRecipe.bind(null, author);
-};
-
-var assignAuthorToNewRecipe = function(author, recipe) {
-  var recipeCopy = {};
-  for (var k in recipe) {
+const assignAuthorToNewRecipe = (author, recipe) => {
+  const recipeCopy = {};
+  Object.keys(recipe).forEach((k) => {
     if (k !== 'id' && k !== 'created_at' && k !== 'updated_at') {
       recipeCopy[k] = recipe[k];
     }
-  }
+  });
   recipeCopy.parent = recipe.id;
   recipeCopy.author = author;
   return recipeCopy;
 };
 
-var createRecipe = function(recipe, callback) {
+const bindAuthorToNewRecipe = (author) => assignAuthorToNewRecipe.bind(null, author);
+
+const createRecipe = (recipe, callback) => {
   fetch('http://localhost:8080/api/v1/recipes/', {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(recipe)
+    body: JSON.stringify(recipe),
   })
-  .then(function(response) {
+  .then((response) => {
     if (response.status >= 400) {
       callback(new Error('Bad response from server'));
     }
     return response.json();
   })
-  .then(function(data) {
+  .then((data) => {
     callback(data);
   })
-  .catch(function(error) {
+  .catch((error) => {
     console.log('Error:', error);
     return;
   });
 };
 
-var forkRecipe = function(originalRecipeId, userId, callback) {
-  fetchRecipe(originalRecipeId, function(recipe) {
+const forkRecipe = (originalRecipeId, userId, callback) => {
+  fetchRecipe(originalRecipeId, (recipe) => {
     createRecipe(bindAuthorToNewRecipe(userId)(recipe), callback);
   });
 };
 
+// forkRecipe(1, 432, console.log);
 
 // FORKS
 
