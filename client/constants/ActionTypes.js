49afcb64655812625f7767311ef6e9018ad70e4f
controtie <dylan.ad.tran@gmail.com>
2016-05-07 06:23:32 -0700

    add actions, reducers, and constants for editRecipe and addField

--- client/constants/ActionTypes.js
+++ client/constants/ActionTypes.js
@@ -2,3 +2,5 @@
 export const UPDATE_RECIPE = 'UPDATE_RECIPE';
 export const TOGGLE_EDIT = 'TOGGLE_EDIT';
 export const FORK_RECIPE = 'FORK_RECIPE';
+export const EDIT_RECIPE = 'EDIT_RECIPE';
+export const ADD_FIELD = 'ADD_FIELD';
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -4,6 +4,7 @@ import { routerReducer } from 'react-router-redux';
 import profile from './profile.js';
 import recipesFeatured from './recipesFeatured.js';
 import recipesFollowed from './recipesFollowed.js';
+import editRecipe from './editRecipe.js';
 import recipesOwned from './recipesOwned.js';
 import recipesSearched from './recipesSearched.js';
 import recipesTop from './recipesTop.js';
@@ -12,6 +13,7 @@ import recipe from './recipe.js';
 
 const rootReducer = combineReducers({
   profile,
+  recipe: editRecipe,
   recipesFeatured,
   recipesFollowed,
   recipesOwned,
