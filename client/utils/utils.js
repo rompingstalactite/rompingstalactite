6854c342746043ed71ce9ed32857040c3b605c06
Andrew H <aho338@gmail.com>
2016-05-11 12:09:40 -0700

    Allow recipes to be accessed without login

--- client/utils/utils.js
+++ client/utils/utils.js
@@ -7,7 +7,6 @@ export const fetchRecipe = (recipeID, callback) => {
       Accept: 'application/json',
       'Content-Type': 'application/json',
     },
-    credentials: 'same-origin',
   })
   .then((response) => {
     if (response.status >= 400) {
