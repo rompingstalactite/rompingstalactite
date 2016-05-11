2f9cd85c6cb3ed5bcbaf7b131fa1fa0af423996a
thomasingalls <thomasingalls@gmail.com>
2016-05-11 16:11:55 -0700

    Add methods for getLikeState, addLikedRecipe, and deleteLikedRecipe

--- server/APIv1/likes/likesController.js
+++ server/APIv1/likes/likesController.js
@@ -5,6 +5,153 @@ const db = pgp(cn);
 
 module.exports = {
 
+  getLikeState: (request, response, next) => {
+    const userID = request.query.userID || request.body.userID;
+    const recipeID = request.query.recipeID || request.body.recipeID;
+    const newQueryObj = {
+      name: 'find-user-recipe-like',
+      text: `WITH recipe_of_interest AS (
+              SELECT
+                user_id
+              FROM
+                likes_recipes_users
+              WHERE
+                recipe_id = $2
+            )
+            SELECT
+              count(*) AS likeCount,
+              (EXISTS
+                (SELECT
+                  1
+                FROM
+                  recipe_of_interest
+                WHERE
+                  user_id = $1)
+              ) AS toggleLike
+            FROM
+              recipe_of_interest;`,
+      values: [userID, recipeID],
+    };
+    db.one(newQueryObj).then(data => {
+      response.json(data);
+      next();
+    }).catch((error) => {
+      response.json(error);
+      next();
+    });
+  },
+  addLikedRecipe: (request, response, next) => {
+    const newQueryObj = {
+      name: 'add-liked-recipe',
+      text: `-- WITH upsert AS (
+               INSERT INTO
+                 likes_recipes_users
+                   (user_id, recipe_id)
+               VALUES
+                 ($1, $2)
+               ON CONFLICT
+                 (user_id, recipe_id)
+               DO NOTHING
+               RETURNING *;
+             -- ),
+             -- // TODO: would be better to select from
+             -- likes count column in recipes table
+             -- and add one to that, since exactness isn't
+             -- important and since it would be faster
+            -- recipe_of_interest AS (
+            --   SELECT
+            --     user_id
+            --   FROM
+            --     likes_recipes_users
+            --   WHERE
+            --     recipe_id = $2
+            -- )
+            -- SELECT
+            --   count(*) AS likeCount,
+            --   (EXISTS
+            --     (SELECT
+            --       1
+            --     FROM
+            --       recipe_of_interest
+            --     WHERE
+            --       user_id = $1)
+            --   ) AS toggleLike
+            -- FROM
+            --   recipe_of_interest;`,
+      values: [
+        request.body.userID,
+        request.body.recipeID,
+      ],
+    };
+
+    db.query(newQueryObj).then(data => {
+      // console.log(data);
+      // response.json(data);
+      module.exports.getLikeState(request, response, next);
+      // next();
+    }).catch((error) => {
+      response.json(error);
+      // possible errors:
+      // recipe does not exist
+      // user does not exist
+      // both recipe and user do not exist
+      // query failed
+      next();
+    });
+  },
+
+  deleteLikedRecipe: (request, response, next) => {
+    const newQueryObj = {
+      name: 'delete-liked-recipe',
+      text: `-- WITH deleting AS (
+               DELETE FROM
+                 likes_recipes_users
+               WHERE user_id = $1
+               AND recipe_id = $2
+               RETURNING *;
+             -- // TODO: would be better to select from
+             -- likes count column in recipes table
+             -- and add one to that, since exactness isn't
+             -- important and since it would be faster
+             -- ),
+             --recipe_of_interest AS (
+             --  SELECT
+             --    user_id
+             --  FROM
+             --    likes_recipes_users
+             --  WHERE
+             --    recipe_id = $2
+             --)
+             --SELECT
+             --  count(*) AS likeCount,
+             --  (EXISTS
+             --    (SELECT
+             --      1
+             --    FROM
+             --      recipe_of_interest
+             --    WHERE
+             --      user_id = $1)
+             --  ) AS toggleLike
+             --FROM
+             --  recipe_of_interest;`,
+      values: [
+        request.body.userID,
+        request.body.recipeID,
+      ],
+    };
+
+    db.query(newQueryObj).then(data => {
+      // console.log(data);
+      // response.json(data);
+      module.exports.getLikeState(request, response, next);
+      // next();
+    }).catch((error) => {
+      console.log(error);
+      // possible errors:
+      // query failed for unknown reason
+      next();
+    });
+  },
 
   getAllLikedRecipes: (request, response, next) => {
     const newQueryObj = {
