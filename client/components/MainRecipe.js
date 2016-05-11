8f91c4b4255c3973f8f2f296a5c0a7299b205e91
Andrew H <aho338@gmail.com>
2016-05-10 17:25:22 -0700

    Replace hardcoded div with RecipeContainer component

--- client/components/MainRecipe.js
+++ client/components/MainRecipe.js
@@ -32,20 +32,27 @@ class MainRecipe extends Component {
         {/*<h1>THIS IS THE USER: {user.displayName}</h1>
         <h1>THIS IS THE RECIPE AUTHOR: {recipe.author}</h1>
         <h1>THIS IS THE RECIPE PARENT: {recipe.parent}</h1>*/}
+        {editButton}
+        {forkButton}
         <div className="recipe-content" contentEditable={toggleEdit}>
           <div className="header">
             <h2 className="recipe-main-title">{recipe.title}</h2>
-            {editButton}
-            {forkButton}
             <div className="header-images">
               <div className="recipe-images">
                 {recipe.images.map((image) => <img src={image} />)}
               </div>
-              <div className="fork-history">
+              {/*<div className="fork-history">
                 <p> - v.1.3 forked May 1st 2016</p>
                 <p> - v.1.2 forked March 12th 2016</p>
                 <p> - v.1.1 forked December 19th 2015</p>
                 <p> - v.1.0 forked December 5th 2015</p>
+              </div>*/}
+              <div>
+                <RecipeContainer
+                  className="fork-history"
+                  type="Recipe History"
+                  recipes={historyRecipes || []}
+                />
               </div>
             </div>
             <h4>Servings: {recipe.yield + ' ' + recipe.yield_unit} </h4>
@@ -86,13 +93,6 @@ class MainRecipe extends Component {
             <h2> Documentation </h2>
           </div>
 
-        </div>
-        <div>
-          <RecipeContainer
-            className="recipe-history"
-            type="Recipe History"
-            recipes={historyRecipes}
-          />
         </div>
       </div>
     );
