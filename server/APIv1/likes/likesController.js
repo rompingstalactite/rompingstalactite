f45deeef6b6162a086dc40269dfca3e1e8157e50
thomasingalls <thomasingalls@gmail.com>
2016-05-11 16:11:02 -0700

    Add getAllLikedRecipes method, to get all liked recipes for a single user

--- server/APIv1/likes/likesController.js
+++ server/APIv1/likes/likesController.js
@@ -1,3 +1,38 @@
 import { postgresConnection as cn } from '../../config/helpers.js';
 const pgp = require('pg-promise')();
 const db = pgp(cn);
+
+
+module.exports = {
+
+
+  getAllLikedRecipes: (request, response, next) => {
+    const newQueryObj = {
+      name: 'get-my-favorite-recipes',
+      text: `SELECT
+              *
+            FROM
+              recipes r INNER JOIN likes_recipes_users lru
+            ON
+              lru.recipe_id = r.id INNER JOIN users u
+            ON
+              lru.user_id = u.id
+            WHERE
+              u.id = $1;`,
+      values: [
+        request.params.user,
+      ],
+    }
+
+    db.query(newQueryObj)
+      .then((data) => {
+        response.status(200);
+        response.json(data);
+        next();
+      })
+      .catch((error) => {
+        console.log(error);
+        next();
+      });
+  }
+};
