6493a164510e10cbb0bd81a095dbf6a6b7164c9b
controtie <dylan.ad.tran@gmail.com>
2016-05-12 12:00:05 -0700

    add logic to properly display images in RecipeEntry

--- client/components/RecipeEntry.js
+++ client/components/RecipeEntry.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { Link } from 'react-router';
 import '../scss/_recipeEntry.scss';
 
-const RecipeEntry = (props) => (
-  <div className="recipe-entry">
-    <img className="recipe-image" src={props.recipe.image} />
-    <Link to={`/recipe/${props.recipe.id}`} className="recipe-title">{props.recipe.title}</Link>
-    <p className="recipe-create-date" >Created at {props.recipe.created_at}</p>
-  </div>
-);
-
+const RecipeEntry = (props) => {
+  let image;
+  if (props.recipe.image) {
+    image = props.recipe.image;
+  } else if (props.recipe.images) {
+    image = props.recipe.images[0];
+  } else {
+    image = null;
+  }
+  return (
+    <div className="recipe-entry">
+      <img className="recipe-image" src={image} />
+      <Link to={`/recipe/${props.recipe.id}`} className="recipe-title">{props.recipe.title}</Link>
+      <p className="recipe-create-date" >Created at {props.recipe.created_at}</p>
+    </div>
+  );
+};
 export default RecipeEntry;
