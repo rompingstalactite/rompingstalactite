fa78a5a92f1dc0eb458b7614586aed8404ad9473
Andrew H <aho338@gmail.com>
2016-05-12 12:04:57 -0700

    Remove console log

--- client/reducers/toggleLike.js
+++ client/reducers/toggleLike.js
@@ -8,7 +8,6 @@ const initialState = {
 export default function toggleLike(state = initialState, action) {
   switch (action.type) {
     case types.TOGGLE_LIKE:
-      console.log("state", state, "action.change", action.change);
       return Object.assign({}, state, action.change);
     default:
       return state;
