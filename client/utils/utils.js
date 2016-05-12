197dd94b3df6fb4c62e9061c6d153e6cd4553af7
controtie <dylan.ad.tran@gmail.com>
2016-05-11 19:09:36 -0700

    hook up frontend search to backend endpoint via searchRecipes util

--- client/utils/utils.js
+++ client/utils/utils.js
@@ -139,6 +139,27 @@ export const fetchRecipes = (recipeIDList, callback) => {
   });
 };
 
+export const searchRecipes = (query, callback) => {
+  const formattedQuery = JSON.stringify(query).replace('[','{').replace(']','}');
+  console.log(formattedQuery);
+  fetch(`http://localhost:8080/api/v1/search/${query}`, {
+    credentials: 'same-origin',
+  })
+  .then((response) => {
+    if (response.status >= 400) {
+      throw new Error('Bad response from server');
+    }
+    return response.json();
+  })
+  .then((data) => {
+    callback(data);
+  })
+  .catch((error) => {
+    console.log('Error:', error);
+    return;
+  });
+};
+
 export const fetchTrending = (callback) => {
   fetch(`http://localhost:8080/api/v1/recipes/trending`, {
     method: 'GET',
