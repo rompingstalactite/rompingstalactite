e02ba91339b9abf1cd1fdbd523ff47a3c6cdb471
controtie <dylan.ad.tran@gmail.com>
2016-05-11 19:08:17 -0700

    allow searchbar to return a list of objects which match the query

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
+import { searchRecipes } from '../utils/utils.js';
 import '../scss/_nav.scss';
 
 class Nav extends Component {
   render() {
-    const { user, avatar, recipeID, } = this.props;
+    const { user, avatar, search, recipeID, } = this.props;
+    let { searchString } = this.props;
     let signInOut;
     if (!user.id) {
       signInOut = <a href="/auth/google">Sign in with Google</a>;
@@ -16,7 +18,20 @@ class Nav extends Component {
       <div>
         <div className="nav-bar">
           <Link to="/">GitCooking</Link>
-          <input placeholder="Search for recipes"></input>
+          <input
+            className="search-bar"
+            placeholder="Search for recipes"
+            onChange={(e) => { searchString = e.target.value; }}
+          ></input>
+          <button
+            onClick={
+              (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                search(searchString);
+              }
+            }
+          > Search </button>
           {signInOut}
           <Link to={`/recipe/${recipeID}`}>Recipe</Link>
           <Link to="/search">Search</Link>
@@ -35,8 +50,22 @@ const mapStateToProps = (state) => {
   return {
     user: state.user,
     avatar: state.user.photos[0].value,
+<<<<<<< 8670c084483f46b26f42a9833befd4882a476e65
     recipeID: state.recipe.id,
+=======
+    searchString: '',
+>>>>>>> allow searchbar to return a list of objects which match the query
   };
 };
 
-export default connect(mapStateToProps)(Nav);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    search: (query) => {
+      searchRecipes(query, (results) => {
+        console.log(results);
+      });
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Nav);
