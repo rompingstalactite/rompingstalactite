bf5e52b44cd966ad723ff0d6bc8fd1559173d0f7
controtie <dylan.ad.tran@gmail.com>
2016-05-13 10:06:32 -0700

    add window check on setting local URL

--- client/utils/utils.js
+++ client/utils/utils.js
@@ -1,6 +1,6 @@
 import 'isomorphic-fetch';
 let localServerURL;
-if (window.location) {
+if (window && window.location) {
   localServerURL = location.origin;
 } else {
   localServerURL = 'http://localhost:8080';
