06fe8abcc18b10a758b35bcdba31be8acba8a4fa
Andrew H <aho338@gmail.com>
2016-05-13 10:15:02 -0700

    Use production version of react in PROD, closes #64

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var webpack = require('webpack');
 
+var PROD = (process.env.NODE_ENV === 'production');
+
 module.exports = {
   entry: './client/index.js',
   output: { path: __dirname, filename: '/client/bundle.js' },
@@ -29,5 +31,17 @@ module.exports = {
     'react/lib/ExecutionEnvironment': true,
     'react/lib/ReactContext': 'window',
     'react/addons': true,
-  }
+  },
+  plugins: PROD ? [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ] : [],
 };
