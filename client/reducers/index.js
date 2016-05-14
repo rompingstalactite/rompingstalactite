ab675ea117eded35a1531acd1b172dd7979bdbca
Andrew H <aho338@gmail.com>
2016-05-13 17:23:33 -0700

    Remove unused reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,8 +2,6 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 import recipesFeatured from './recipesFeatured.js';
-import recipesFollowed from './recipesFollowed.js';
-import recipesOwned from './recipesOwned.js';
 import recipesSearched from './recipesSearched.js';
 import recipesTop from './recipesTop.js';
 import toggleEdit from './toggleEdit.js';
@@ -14,8 +12,6 @@ import toggleLike from './toggleLike.js';
 
 const rootReducer = combineReducers({
   recipesFeatured,
-  recipesFollowed,
-  recipesOwned,
   recipesSearched,
   recipesTop,
   toggleEdit,
