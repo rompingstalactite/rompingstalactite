50b4d18ebe6a8115dd187b7e466b21ad02333ddd
Andrew H <aho338@gmail.com>
2016-05-13 17:40:49 -0700

    Make existing tests pass

--- test/components/fakeStore.test.js
+++ test/components/fakeStore.test.js
@@ -7,8 +7,6 @@ import fakeStore from './fakeStore';
 describe ('::Fake Store::', () => {
   it('should have object as State', () => {
     let state = fakeStore.getState();
-    expect(state).to.have.property('recipesOwned');
-    expect(state).to.have.property('recipesFollowed');
     expect(state).to.have.property('recipesFeatured');
     expect(state).to.have.property('recipesTop');
   });
