7fa76428922af2ccb5c485928522b3f9d5414b90
Andrew H <aho338@gmail.com>
2016-05-13 17:16:21 -0700

    Comment out unused reducers

--- client/reducers/recipesFollowed.js
+++ client/reducers/recipesFollowed.js
@@ -6,16 +6,16 @@
 // SEED DATA FOR TESTING
 // =====
 
-const initialState = [
-  { title: 'Followed Recipe 1' },
-  { title: 'Followed Recipe 2' },
-  { title: 'Followed Recipe 3' }];
-
-export default function recipesFollowed(state = initialState /*, action*/) {
-  // switch (action.type) {
-  // case types.UPDATE_RECIPES:
-  //   return state.concat(action.recipes);
-  // default:
-  return state;
-  // }
-}
+// const initialState = [
+//   { title: 'Followed Recipe 1' },
+//   { title: 'Followed Recipe 2' },
+//   { title: 'Followed Recipe 3' }];
+//
+// export default function recipesFollowed(state = initialState /*, action*/) {
+//   // switch (action.type) {
+//   // case types.UPDATE_RECIPES:
+//   //   return state.concat(action.recipes);
+//   // default:
+//   return state;
+//   // }
+// }
--- client/reducers/recipesOwned.js
+++ client/reducers/recipesOwned.js
@@ -6,16 +6,16 @@
 // SEED DATA FOR TESTING
 // =====
 
-const initialState = [
-  { title: 'Owned Recipe 1' },
-  { title: 'Owned Recipe 2' },
-  { title: 'Owned Recipe 3' }];
-
-export default function recipesOwned(state = initialState /*, action*/) {
-  // switch (action.type) {
-  // case types.UPDATE_RECIPES:
-  //   return state.concat(action.recipes);
-  // default:
-  return state;
-  // }
-}
+// const initialState = [
+//   { title: 'Owned Recipe 1' },
+//   { title: 'Owned Recipe 2' },
+//   { title: 'Owned Recipe 3' }];
+//
+// export default function recipesOwned(state = initialState /*, action*/) {
+//   // switch (action.type) {
+//   // case types.UPDATE_RECIPES:
+//   //   return state.concat(action.recipes);
+//   // default:
+//   return state;
+//   // }
+// }
