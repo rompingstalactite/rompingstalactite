406a3c6e60e0938ead30f66f1d2fd705f771f041
Andrew H <aho338@gmail.com>
2016-05-14 15:05:27 -0700

    Move express secrets to separate file

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -5,16 +5,18 @@ import passport from 'passport';
 import session from 'express-session';
 
 let expressSessionSecret = process.env.EXPRESS_SESSION_SECRET;
+let cookieParserSecret = process.env.COOKIE_PARSER_SECRET;
 
 if (!process.env.TRAVIS && !process.env.HEROKU) {
-  expressSessionSecret = require('../keys/expressSession').expressSessionSecret.EXPRESS_SESSION_SECRET;
+  expressSessionSecret = require('../keys/expressSecrets').expressSecret.EXPRESS_SESSION_SECRET;
+  cookieParserSecret = require('../keys/expressSecrets').expressSecret.COOKIE_PARSER_SECRET;
 }
 
 module.exports = (app, express) => {
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
-  app.use(cookieParser('githubForRecipes'));
+  app.use(cookieParser(cookieParserSecret));
   app.use(passport.initialize());
   app.use(session({
     secret: expressSessionSecret,
--- server/keys/expressSession.example.js
+++
@@ -1,3 +0,0 @@
-export const expressSessionSecret = {
-  EXPRESS_SESSION_SECRET: 'YOUR EXPRESS SESSION SECRET',
-};
