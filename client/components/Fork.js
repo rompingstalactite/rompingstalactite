0c95c3440e25061d97daecbd7605e19f4d2d24ef
Andrew H <aho338@gmail.com>
2016-05-14 15:48:08 -0700

    Make Fork its own component

--- client/components/Fork.js
+++ client/components/Fork.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
+import actions from '../actions/index.js';
+import { forkRecipe } from '../utils/utils';
 
 const Fork = (props) => {
-  return (
-    <div className="btn-fork">
-    </div>
-  );
+  let forkComponent;
+  if (props.userID) {
+    forkComponent = (<button className="btn-fork" onClick={ props.onForkClick.bind(null, props.recipeID, props.userID) }>
+      Fork
+    </button>);
+  } else {
+    forkComponent = (<button className="btn-fork" disabled>Fork</button>);
+  }
+  return forkComponent;
 };
-export default Fork;
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onForkClick: (recipeID, userID) => {
+      forkRecipe(recipeID, userID, (newRecipe) => {
+        dispatch(actions.forkRecipe(newRecipe));
+        dispatch(push(`/recipe/${newRecipe.id}`));
+      });
+    },
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Fork);
