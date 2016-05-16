21852bc578017ee559dee29bc294e43e2c14faf4
nemobaker <nemobaker@gmail.com>
2016-05-16 15:16:47 -0700

    remove console logs

--- test/server/recipeController.test.js
+++ test/server/recipeController.test.js
@@ -18,7 +18,6 @@ describe('Recipe Controller results', () => {
       app.request.body.newURL = 'testURLs.COM';
 
       const cb = () => {
-        // console.log('*****ADD RESP*****',app.response.json())
         // done callback fails if comparison fails. WUT
         // expect(app.response.json().length).to.equal(2);
         expect(app.response.json().length).to.be.defined;
@@ -37,7 +36,8 @@ describe('Recipe Controller results', () => {
 
         // done callback fails if comparison fails. WUT
         // expect(app.response.json().length).to.equal(2);
-        expect(app.response.json().length).to.equal(0);
+        console.log('RESPONSE*********', app.response.json())
+        expect(app.response.json().length).to.equal(1);
         done();
       };
 
