a27f0313356287605e808957f20e6dd47b51a92e
Andrew H <aho338@gmail.com>
2016-05-16 15:06:46 -0700

    Change class name for recipe entry title

--- client/components/RecipeListEntry.js
+++ client/components/RecipeListEntry.js
@@ -8,7 +8,7 @@ const RecipeListEntry = (props) => {
   const createdTime = moment(props.recipe.created_at).fromNow();
   return (
     <div className="recipe-list-entry">
-      <Link to={`/recipe/${props.recipe.recipe_id || props.recipe.id || 1}`} className="recipe-title">{props.recipe.title}</Link>
+      <Link to={`/recipe/${props.recipe.recipe_id || props.recipe.id || 1}`} className="recipe-entry-title">{props.recipe.title}</Link>
       <p className="recipe-author" >Created by {props.recipe.display_name}</p>
       <p className="recipe-create-date" >Created at {createdTime}</p>
     </div>
