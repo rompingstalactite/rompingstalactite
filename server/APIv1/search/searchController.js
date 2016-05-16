dff9d13b7508fe6e7abc6220b25bf924d1aca21b
nemobaker <nemobaker@gmail.com>
2016-05-16 10:31:09 -0700

    reimplement title/ingredient search

--- server/APIv1/search/searchController.js
+++ server/APIv1/search/searchController.js
@@ -5,55 +5,78 @@ const db = pgp(cn);
 
 module.exports = {
   searchRecipes: (request, response, next) => {
-    // munge space-separated words into a single OR-separated string
-    // NOTE: this *may* be breaking query sanitization provided by pg-promise
-    if (!request.body) {
-      request.body = {};
-    } else {
-      request.body._queryResultIds = [];
-    }
-
-    const query = decodeURIComponent(request.params.q)
-      .split(' ')
-      .join(' | ');
-
     const queryObj = {
-      name: 'search-recipes-table',
-      text: `SELECT
-               *
-             FROM (
-               SELECT
-                 to_tsvector(CAST(recipes.id as VARCHAR)) || ' ' ||
-                 to_tsvector(CAST(recipes.ingredients as VARCHAR)) || ' ' ||
-                 to_tsvector(CAST(recipes.cook_steps as VARCHAR)) || ' ' ||
-                 to_tsvector(CAST(recipes.prep_steps as VARCHAR)) || ' ' ||
-                 to_tsvector(CAST(recipes.finish_steps as VARCHAR)) || ' ' ||
-                 to_tsvector(coalesce((string_agg(recipes.title, ' ')), ''))
-                   as document
-               FROM
-                 recipes
-               GROUP BY
-                 recipes.id
-             ) p_search
-             WHERE
-               p_search.document @@ to_tsquery($1);`,
-      values: [query],
+      name: 'search-recipes',
+      text: 'SELECT id FROM recipes WHERE exists (SELECT TITLE FROM (SELECT UNNEST(recipes.ingredients)) x(recipe) where x.recipe like '%cup%')',
+      values: [request.body.title, request.body.prep_steps, request.body.cook_steps],
     };
 
-    db.query(queryObj)
+    console.log('REQUEST.BODY****************',request.body)
+    db.one(queryObj)
       .then((data) => {
-        response.status(201);
-        request.body.recipes = data.map((val) => {
-          return parseInt(val.document.match(/(?:'1':)(\d+)(?=[,\s])/)[1], 10);
-        });
-        // console.log(request.body._queryResultIds);
-
-        return getMultipleRecipes(request, response, next);
+        response.status(200);
+        response.json(data);
+        next();
       })
       .catch((error) => {
-        // console.log('query error', error)
-        response.json(error);
+        response.status(500);
+        response.json({
+          error: `Error code: ${error.code}, Error message: ${error.detail}`,
+        });
         next();
       });
   },
+
+  // searchRecipes: (request, response, next) => {
+  //   // munge space-separated words into a single OR-separated string
+  //   // NOTE: this *may* be breaking query sanitization provided by pg-promise
+  //   if (!request.body) {
+  //     request.body = {};
+  //   } else {
+  //     request.body._queryResultIds = [];
+  //   }
+  //
+  //   const query = decodeURIComponent(request.params.q)
+  //     .split(' ')
+  //     .join(' | ');
+  //
+  //   const queryObj = {
+  //     name: 'search-recipes-table',
+  //     text: `SELECT
+  //              *
+  //            FROM (
+  //              SELECT
+  //                to_tsvector(CAST(recipes.id as VARCHAR)) || ' ' ||
+  //                to_tsvector(CAST(recipes.ingredients as VARCHAR)) || ' ' ||
+  //                to_tsvector(CAST(recipes.cook_steps as VARCHAR)) || ' ' ||
+  //                to_tsvector(CAST(recipes.prep_steps as VARCHAR)) || ' ' ||
+  //                to_tsvector(CAST(recipes.finish_steps as VARCHAR)) || ' ' ||
+  //                to_tsvector(coalesce((string_agg(recipes.title, ' ')), ''))
+  //                  as document
+  //              FROM
+  //                recipes
+  //              GROUP BY
+  //                recipes.id
+  //            ) p_search
+  //            WHERE
+  //              p_search.document @@ to_tsquery($1);`,
+  //     values: [query],
+  //   };
+  //
+  //   db.query(queryObj)
+  //     .then((data) => {
+  //       response.status(201);
+  //       request.body.recipes = data.map((val) => {
+  //         return parseInt(val.document.match(/(?:'1':)(\d+)(?=[,\s])/)[1], 10);
+  //       });
+  //       // console.log(request.body._queryResultIds);
+  //
+  //       return getMultipleRecipes(request, response, next);
+  //     })
+  //     .catch((error) => {
+  //       // console.log('query error', error)
+  //       response.json(error);
+  //       next();
+  //     });
+  // },
 };
