3f9fe7e4ae4dd4faed0e4d9e7c13ea1eb8d7dd4b
controtie <dylan.ad.tran@gmail.com>
2016-05-17 12:16:52 -0700

    fix up search bar and remove extra nav elements

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -9,11 +9,11 @@ import '../scss/_nav.scss';
 
 class Nav extends Component {
   render() {
-    const { user, avatar, search, recipeID, navToCreate } = this.props;
+    const { user, avatar, search, recipeID, navToCreate, dispatch } = this.props;
     let { searchString } = this.props;
     let signInOut, linkToProfile;
     if (!user.id) {
-      signInOut = <a href="/auth/google">Sign in with Google</a>;
+      signInOut = <button onClick={dispatch(push('/auth/google/'))}> Sign In </button>
     } else {
       signInOut = <a href="/auth/signout">Sign out</a>;
       linkToProfile = (
@@ -24,32 +24,34 @@ class Nav extends Component {
     return (
       <div>
         <div className="nav-bar">
-          <Link to="/">GitCooking</Link>
-          <input
-            className="search-bar"
-            placeholder="Search for recipes"
-            onChange={(e) => { searchString = e.target.value; }}
-          ></input>
-          <button
-            onClick={
-              (e) => {
+          <div className="nav-bar-left">
+            <Link to="/">GitCooking</Link>
+            <input
+              className="search-bar"
+              placeholder="Search for recipes"
+              onChange={(e) => { searchString = e.target.value; }}
+            ></input>
+            <button
+              onClick={(e) => {
                 e.preventDefault();
                 e.stopPropagation();
                 search(searchString);
-              }
-            }
-          > Search </button>
-          <Link to={`/recipe/${recipeID}`}>Recipe</Link>
-          <Link to="/search">Search</Link>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-              navToCreate();
-            }}> Create
-          </button>
-          {signInOut}
-          {linkToProfile}
+              }}
+            > Search </button>
+            <a> Discover </a>
+          </div>
+          <div className="nav-bar-right">
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                navToCreate();
+              }}> +
+            </button>
+            {signInOut}
+            {linkToProfile}
+          </div>
+          {/* <Link to={`/recipe/${recipeID}`}>Recipe</Link>*/}
         </div>
       </div>
     );
@@ -78,6 +80,7 @@ const mapDispatchToProps = (dispatch) => {
       dispatch(actions.setRecipe(EMPTY_RECIPE));
       dispatch(push('/create'));
     },
+    dispatch,
   };
 };
 
