6bee44141ab2d92efd29f64b980d21487d297b42
thomasingalls <thomasingalls@gmail.com>
2016-05-17 18:51:56 -0700

    Add sql query for adding user follows

--- server/APIv1/follows/addUserFollow.sql
+++ server/APIv1/follows/addUserFollow.sql
@@ -2,7 +2,8 @@ INSERT INTO
   followers_users_users
     (follower, target)
 VALUES
-  (${currentUser}, ${target})
+  (${userID}, ${targetID})
+  -- (${currentUser}, ${target})
 ON CONFLICT
   (follower, target)
 DO NOTHING
