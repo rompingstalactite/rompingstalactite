b4cc14b007deae8b0ff73154aa337910cbab31f7
thomasingalls <thomasingalls@gmail.com>
2016-05-17 20:12:22 -0700

    Duplicate search results page as discover

---
+++ client/components/Discover.js
@@ -0,0 +1,52 @@
+import React, { Component} from 'react';
+import { connect } from 'react-redux';
+import RecipeContainer from '../../client/components/RecipeContainer.js';
+import actions from '../actions/index.js';
+import { fetchTrending } from '../utils/utils';
+
+import '../scss/_dashboard.scss';
+
+
+class Dashboard extends Component {
+  componentDidMount() {
+    const { handleRecipesTop } = this.props;
+    handleRecipesTop();
+  }
+
+  render() {
+    const { recipesFeatured, recipesTop } = this.props;
+    return (
+      <div className="dashboard">
+        <h1>Dashboard</h1>
+        <RecipeContainer
+          className="recipes-featured"
+          type="Featured Recipes"
+          recipes={recipesFeatured}
+        />
+        <RecipeContainer
+          className="recipes-top"
+          type="Most Forked Recipes"
+          recipes={ recipesTop }
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    recipesFeatured: state.recipesFeatured,
+    recipesTop: state.recipesTop,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleRecipesTop: () => fetchTrending((data) => dispatch(actions.recipesTop(data))),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Dashboard);
