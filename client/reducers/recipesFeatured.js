3d67679bd3c0162650194829335de397bb92d01f
controtie <dylan.ad.tran@gmail.com>
2016-05-17 18:52:11 -0700

    add more fake initial data for featured Recipes

--- client/reducers/recipesFeatured.js
+++ client/reducers/recipesFeatured.js
@@ -22,6 +22,41 @@ const initialState = [
     image: 'http://images.media-allrecipes.com/userphotos/720x405/3554842.jpg',
     display_name: 'GitCooking Staff',
   },
+  {
+    title: 'Vanilla Ice Cream',
+    image: 'http://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/47/90/picDnUZU9.jpg',
+    display_name: 'GitCooking Staff',
+  },
+  {
+    title: 'Cake Batter Ice Cream',
+    image: 'http://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/12/89/52/f15xnUJJQc2kJB0O1v6o_birthday-cake-ice-cream-7207.jpg',
+    display_name: 'GitCooking Staff',
+  },
+  {
+    title: 'Red Velvet Cupcakes',
+    image: 'http://40aprons.com/wp-content/uploads/2013/10/red_velvet_cupcakes+2+of+81.jpg',
+    display_name: 'GitCooking Staff',
+  },
+  {
+    title: 'Best Steak Marinade in Existence',
+    image: 'http://images.media-allrecipes.com/userphotos/250x250/225844.jpg',
+    display_name: 'GitCooking Staff',
+  },
+  {
+    title: 'Couscous Primavera',
+    image: 'http://images.media-allrecipes.com/userphotos/720x405/3554842.jpg',
+    display_name: 'GitCooking Staff',
+  },
+  {
+    title: 'Vanilla Ice Cream',
+    image: 'http://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/47/90/picDnUZU9.jpg',
+    display_name: 'GitCooking Staff',
+  },
+  {
+    title: 'Cake Batter Ice Cream',
+    image: 'http://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/12/89/52/f15xnUJJQc2kJB0O1v6o_birthday-cake-ice-cream-7207.jpg',
+    display_name: 'GitCooking Staff',
+  },
 ];
 
 export default function recipesFeatured(state = initialState /*, action*/) {
