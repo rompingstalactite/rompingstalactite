a3c1720064f6cc36c26c73d5ffebd434e0b9e60d
controtie <dylan.ad.tran@gmail.com>
2016-05-18 14:26:47 -0700

    add basic styling + data integration for forkHistoryVis

--- client/components/ForkHistoryVis.js
+++ client/components/ForkHistoryVis.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import actions from '../actions/index.js';
-import { fetchFPKey } from '../utils/utils';
 import '../scss/_forkHistoryVis.scss';
 
 
-const ForkHistoryVis = (props) => {
-  const history = props.history;
-  const recipe = props.recipe;
-  return (
-    <div className="history-vis">
-      <div className="history-vis-node"> </div>
-      <div className="history-vis-edge"> </div>
-      <div className="history-vis-node"> </div>
-      <div className="history-vis-edge"> </div>
-      <div className="history-vis-node"> </div>
-      <div className="history-vis-edge"> </div>
-      <div className="history-vis-node"> </div>
-    </div>
-  );
-};
+class ForkHistoryVis extends Component {
+  constructor() {
+    super();
+  }
+  componentDidUpdate() {
+    const { history, recipe } = this.props;
+    console.log('props', this.props);
+    console.log('mounted history', history);
+    console.log('mounted recipe', recipe);
+  }
+  render() {
+    const { history, recipe } = this.props;
+    let recipeHistory = <p> no history </p>;
+    if (history && history.length > 0) {
+      recipeHistory = <div>
+      {history.map(recipe => <div>
+        <div className="history-vis-edge"> </div>
+          <div className="history-vis-node" data-recipeInfo={recipe.title}>
+            <div className="tooltiptext"> {recipe.title} <br /> {'Created: ' + recipe.created_at.slice(0, 10)}</div>
+          </div>
+        </div>)}
+      </div>;
+    }
+    return (
+      <div className="history-vis">
+        <div className="history-vis-node" data-recipeInfo={recipe}>
+          <span className="tooltiptext"> Current Recipe </span>
+        </div>
+        {recipeHistory}
+      </div>
+    );
+  }
+}
+        // <button onClick={() => console.log(history)}> Get History </button>
 
 export default ForkHistoryVis;
