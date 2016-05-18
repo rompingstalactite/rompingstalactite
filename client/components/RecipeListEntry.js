310cf1dbb51f48e73a8109577f40fb8177d5af42
controtie <dylan.ad.tran@gmail.com>
2016-05-18 16:25:02 -0700

    Add styling changes to MainRecipe View

--- client/components/RecipeListEntry.js
+++ client/components/RecipeListEntry.js
@@ -10,6 +10,7 @@ const RecipeListEntry = (props) => {
   const createdTime = moment(props.recipe.created_at).fromNow();
   return (
     <div className="recipe-list-entry">
+      <img src={props.recipe.images[0]} />
       <Link to={`/recipe/${props.recipe.recipe_id || props.recipe.id || 1}`} className="recipe-entry-title">{props.recipe.title}</Link>
       <p className="recipe-author" >Created by {props.recipe.display_name}</p>
       <p className="recipe-create-date" >Created at {createdTime}</p>
