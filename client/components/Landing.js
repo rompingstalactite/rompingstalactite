b7f8012208b7a1bc791277ed9602b0f492477da0
Andrew H <aho338@gmail.com>
2016-05-18 12:15:17 -0700

    Create first iteration of landing page

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -21,34 +21,50 @@ let heroImageStyle = {
   backgroundSize: 'cover',
   backgroundRepeat: 'no-repeat',
   backgroundPosition: 'center center',
-  height: '500px',
+  // height: '500px',
+  paddingTop: '100px',
 };
 
 const Landing = () => (
   <div className="landing">
     <div className="hero" style={heroImageStyle}>
-      <h1 style={{color: 'white', textAlign: 'center'}}>[LOGO]</h1>
-      <a href="/auth/google" style={{color: 'white', textAlign: 'center'}}>
-        <img className="google-signin" src="../assets/btn_google_signin_light_normal_web@2x.png" />
-      </a>
-      <a href="/dashboard">
-        <h3 style={{color: 'white', textAlign: 'center'}}>Continue without signing in</h3>
-      </a>
+      <div className="hero-content grid grid-pad">
+        <div className="hero-content-logo col-1-3">
+          <img style={{display: 'block', margin: 'auto'}} className="landing-content-image" src="http://placehold.it/200x200"></img>
+        </div>
+        <div style={{marginTop: '48px'}} className="hero-content-text">
+          <h2 style={{color: 'white', marginBottom: '0px'}} className="hero-tagline col-2-3">Cook. Iterate. Repeat.</h2>
+          <h3 style={{color: 'white', marginTop: '8px', fontWeight: 'normal'}} className="hero-tagline col-2-3">GitCooking helps you perfect your next meal by tracking different iterations of recipes.</h3>
+        </div>
+        <div style={{margin: '40px 10px'}} className="hero-content-signin col-1-1">
+          <a href="/auth/google" style={{color: 'white', textAlign: 'center'}}>
+            <img style={{display: 'block', margin: 'auto'}} className="google-signin" src="../assets/btn_google_signin_light_normal_web@2x.png" />
+          </a>
+          <a href="/dashboard">
+            <h3 style={{color: 'white', textAlign: 'center'}}>or, continue without signing in</h3>
+          </a>
+        </div>
+      </div>
     </div>
-    <div className="landing-content">
-      <div className="landing-content-blurb">
-        <img className="landing-content-image" src="http://placehold.it/200x200"></img>
-        <p className="landing-content-text">Sriracha fixie heirloom slow-carb XOXO chartreuse locavore, mixtape cornhole whatever vice fashion axe. Semiotics biodiesel beard pinterest fap, post-ironic chambray austin lo-fi taxidermy brunch pabst hella etsy green juice. Migas pitchfork plaid gastropub keffiyeh, celiac taxidermy kitsch. Williamsburg letterpress kickstarter single-origin coffee marfa. Cray seitan blue bottle, franzen deep v poutine cronut narwhal polaroid irony hashtag flexitarian wolf gochujang. Chicharrones +1 dreamcatcher, disrupt blog man braid PBR&B godard locavore four loko single-origin coffee yuccie. Jean shorts raw denim plaid drinking vinegar, hammock venmo synth.</p>
+    <div className="grid grid-pad landing-content">
+      <div style={{marginBottom: '48px'}} className="col-1-3 landing-content-blurb">
+        <h3 style={{textAlign: 'center'}} className="landing-content-title">Fork Recipes</h3>
+        <p style={{height: '24px', textAlign: 'center', marginBottom: '8px'}} className="landing-content-text">"Fork" a recipe to modify it and make it your own.</p>
+        <img style={{width: '150px', display: 'block', margin: 'auto'}} className="landing-content-image" src="../assets/fork.png"></img>
       </div>
-      <div className="landing-content-blurb">
-        <p className="landing-content-text">Whatever direct trade synth kinfolk. Plaid wolf truffaut, vice sartorial knausgaard chillwave YOLO lomo kombucha fixie. Blue bottle PBR&B hoodie aesthetic williamsburg austin. Pour-over shabby chic vinyl retro normcore green juice listicle stumptown, pickled pitchfork bitters blue bottle deep v keffiyeh schlitz. Meh lo-fi literally chicharrones, ugh kitsch gluten-free kale chips migas gentrify asymmetrical listicle lomo knausgaard. Hoodie butcher selfies selvage, hashtag taxidermy meggings polaroid schlitz beard kitsch fixie put a bird on it. Yuccie four loko plaid, knausgaard letterpress austin semiotics listicle narwhal lumbersexual cardigan franzen heirloom.</p>
-        <img className="landing-content-image" src="http://placehold.it/200x200"></img>
-
+      <div style={{marginBottom: '48px'}} className="col-1-3 landing-content-blurb">
+        <h3 style={{textAlign: 'center'}} className="landing-content-title">Recipe History</h3>
+        <p style={{height: '24px', textAlign: 'center', marginBottom: '8px'}} className="landing-content-text">See how recipes have changed over time.</p>
+        <img style={{width: '150px', display: 'block', margin: 'auto'}} className="landing-content-image" src="../assets/history.png"></img>
       </div>
-      <div className="landing-content-blurb">
-        <p className="landing-content-text">Lumbersexual polaroid put a bird on it, portland cornhole church-key celiac. Cray photo booth vinyl, fanny pack master cleanse affogato messenger bag viral mustache fashion axe migas. Man braid 3 wolf moon kombucha, tacos pickled butcher YOLO jean shorts pinterest salvia kinfolk meditation +1 microdosing. Franzen ethical ramps neutra, meggings tousled chillwave affogato vice humblebrag poutine. Bushwick meh green juice kitsch hammock small batch. +1 fixie stumptown, kinfolk chicharrones four loko gochujang actually knausgaard tilde. Selfies heirloom pinterest iPhone before they sold out.</p>
-        <img className="landing-content-image" src="http://placehold.it/200x200"></img>
+      <div style={{marginBottom: '48px'}} className="col-1-3 landing-content-blurb">
+        <h3 style={{textAlign: 'center'}} className="landing-content-title">Explore</h3>
+        <p style={{height: '24px', textAlign: 'center', marginBottom: '8px'}} className="landing-content-text">Browse our most popular recipes and get cooking!</p>
+        <img style={{width: '150px', display: 'block', margin: 'auto'}} className="landing-content-image" src="../assets/explore.png"></img>
       </div>
+    </div>
+    <div className="footer">
+      <p style={{textAlign: 'center'}}>Made by Andrew, Dylan, Nemo, and Thomas.</p>
     </div>
   </div>
 );
