1a5994e7f7dc81562da93f4553785b265768d8ae
controtie <dylan.ad.tran@gmail.com>
2016-05-18 19:11:13 -0700

    refactor ForkHistVis and RecipeListEntry to account for null image fields

--- client/components/ForkHistoryVis.js
+++ client/components/ForkHistoryVis.js
@@ -10,21 +10,25 @@ class ForkHistoryVis extends Component {
   }
   componentDidUpdate() {
     const { history, recipe } = this.props;
-    console.log('props', this.props);
-    console.log('mounted history', history);
-    console.log('mounted recipe', recipe);
   }
   render() {
     const { history, recipe } = this.props;
     let recipeHistory = <p> no history </p>;
     if (history && history.length > 0) {
       recipeHistory = <div>
-      {history.map(recipe => <div>
+      {history.map(histRecipe => {
+        let creationDate = 'no date specified';
+        if (histRecipe.created_at) {
+          creationDate = 'Created: ' + histRecipe.created_at.slice(0, 10);
+        }
+        return (<div>
         <div className="history-vis-edge"> </div>
-          <div className="history-vis-node" data-recipeInfo={recipe.title}>
-            <div className="tooltiptext"> {recipe.title} <br /> {'Created: ' + recipe.created_at.slice(0, 10)}</div>
+          <div className="history-vis-node" data-recipeInfo={histRecipe.title}>
+            <div className="tooltiptext"> {histRecipe.title} <br /> {creationDate}</div>
           </div>
-        </div>)}
+        </div>
+        )}
+      )}
       </div>;
     }
     return (
