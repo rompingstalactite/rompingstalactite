157d21134e41c9d20bd65f57fbed9b2daac24058
Andrew H <aho338@gmail.com>
2016-06-11 16:43:03 -0700

    Revise footer styling

--- client/components/App.js
+++ client/components/App.js
@@ -20,7 +20,16 @@ class App extends Component {
       <div>
         <Nav />
         <div>{ children }</div>
-        <div className="footer" style={{height: '50px'}}></div>
+        <div className="footer">
+          <p className="text-muted text-center" style={{paddingTop: '10px', paddingBottom: '10px', margin: 0}}><small>Made by&nbsp;
+            <a href="https://github.com/andrewh0">Andrew</a>,&nbsp;
+            <a href="https://github.com/controtie">Dylan</a>,&nbsp;
+            <a href="https://github.com/nemobaker">Nemo</a>, and&nbsp;
+            <a href="https://github.com/thomasingalls">Thomas</a> in 2016.
+            Recipes powered by <a href="http://www.yummly.com">Yummly</a>.
+            Icons from <a href="http://www.thenounproject.com">The Noun Project</a>.
+          </small></p>
+        </div>
       </div>
     );
   }
